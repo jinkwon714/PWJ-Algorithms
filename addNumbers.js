e06544293a26c28e6e7b0d84f09e6ffc6404bf58
Hyukjin Kwon <hjnim0909@gmail.com>
2020-11-04 13:14:34 +0900

    #2. Convert Minutes to Seconds
    * Learned how to write a function to convertminutes to seconds / seconds to minuites

--- addNumbers.js
+++ addNumbers.js
@@ -4,6 +4,20 @@ function addNumbers(num1, num2) {
     return num1 + num2;
 }
 
-console.log(addNumbers(4,7));
-console.log(addNumbers(6,5));
-console.log(addNumbers(10,7));
+// console.log(addNumbers(4,7));
+// console.log(addNumbers(6,5));
+// console.log(addNumbers(10,7));
+
+
+function convertMinutesToSeconds(minutes) {
+    return minutes * 60;
+}
+
+console.log(convertMinutesToSeconds(1))
+console.log(convertMinutesToSeconds(3))
+
+
+function convertSecondsToMinutes(seconds) {
+    return seconds / 60;
+}
+console.log(convertSecondsToMinutes(60))
