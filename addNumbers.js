a55e54d7725f03c4288c81c18ea754c5c8cfb99d
Hyukjin Kwon <hjnim0909@gmail.com>
2020-11-04 09:39:57 +0900

    #1. Add Two Numbers
    * Concept: how to write a code for getting A+B = ??

---
+++ addNumbers.js
@@ -0,0 +1,9 @@
+console.log("helloe world");
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(addNumbers(4,7));
+console.log(addNumbers(6,5));
+console.log(addNumbers(10,7));
